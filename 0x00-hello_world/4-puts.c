41e8434eddff8147d377ade167913df40275ff62
enochgreat <enogreat18@gmail.com>
2023-09-07 19:46:17 -0700

    put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<studio.h>
+
+/**
+ *main - Hello, puts
+ *
+ *Return: print out the message
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle"\n);
+	return(0);
+}

de8db1ef5642be9d4bc1173032ab4a90200f5d39
enochgreat <enogreat18@gmail.com>
2023-09-10 11:34:05 -0700

    a-z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Print all the alphabet
+ * Return: successful 0
+ */
+	int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
+

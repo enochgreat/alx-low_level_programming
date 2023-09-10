12260934efd11e3b32f0f6e66b27957f094042b6
enochgreat <enogreat18@gmail.com>
2023-09-10 12:22:02 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - alphabet in both upper and lower
+ * Return: success 0
+ */
+	int main(void)
+{	
+	char c;
+	c = 'a' == 'A';
+	while
+		(c <= 'z' && 'Z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+
+	return (0);
+}
+
+

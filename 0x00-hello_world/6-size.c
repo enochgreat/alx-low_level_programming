f966ce1b17a60fa3e4df8f2c72f757d540688c72
enochgreat <enogreat18@gmail.com>
2023-09-10 09:27:17 -0700

    6 size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 #include <limits.h>
 /*
- * main - Size is not granular
- * Return: Always 0 (Success)
+ *main - Size is not grandeur, and territory does not make a nation
+ *return: return 0
  */
-int main(void)
+	int main(void)
 {
-char c;
-int i;
-float f;
-double l;
+	char c;
+	int i;
+	float f;
+	double l;
 
-printf("size of a char: %lu byte(s)\n", sizeof(c));
-printf("size of an int: %lu byte(s)\n", sizeof(i));
-printf("size of a long int: %lu byte(s)\n", sizeof(i));
-printf("size of a long long int: %lu byte(s)\n", sizeof(l));
-printf("size of a float: %lu byte(s)\n", sizeof(f));
-
-return (0);
+	printf("size of a char: %lu byte(s)\n", sizeof(c));
+	printf("size of an int: %lu byte(s)\n", sizeof(i));
+	printf("size of a long int: %lu byte(s)\n", sizeof(i));
+	printf("size of a long long int: %lu byte(s)\n", sizeof(l));
+	printf("size of a float: %lu byte(s)\n", sizeof(f));
+	return (0);
 }
 

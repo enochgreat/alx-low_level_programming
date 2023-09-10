8c52c6f73d81e29d0561936e27d5c6324085309a
enochgreat <enogreat18@gmail.com>
2023-09-10 05:38:26 -0700

    6 size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
+#include <limit.h>
 /*
  *main - Size is not granular
  *Return: success 0
  */
-
 int main(void)
 {
 	char c;
 	int i;
 	float f;
+	double l;
 
-	printf("size of a %c: %u byte(s)\n"sizeof(c)c);
-	printf("size of an %d: %d byte(s)\n"sizeof(i)i);
-	printf("size of a %lu: %lu byte(s)\n"sizeof(i)i);
-	printf("size of a %lld: %lld byte(s)\n"sizeof(i)i);
-	printf("size of a %f: %f byte(s)\n"sizeof(f)f);
+	printf("size of a char: %u byte(s)\n"sizeof(c));
+	printf("size of an int: %d byte(s)\n"sizeof(i));
+	printf("size of a long int: %lu byte(s)\n"sizeof(i));
+	printf("size of a long long int: %lld byte(s)\n"sizeof(l));
+	printf("size of a float: %f byte(s)\n"sizeof(f));
 	return (0);
 }

2c91e0c31628d11e770a346ca68dcdb8f78dee17
enochgreat <enogreat18@gmail.com>
2023-09-10 17:59:59 -0700

    putchar

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
  * main - Putchar variables
- *
  * Return: success 0
  */
- 	int main(void)
+	int main(void)
 {
 	int i;
 
 	i = 0;
-	while(i < 10) {
+	while
+		(i < 10) {
 		putchar(i + '0');
 		i++;
 	}

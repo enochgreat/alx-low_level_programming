ff437f4344533bd5c7d812bb46f6b431f87e73e2
enochgreat <enogreat18@gmail.com>
2023-09-10 17:52:36 -0700

    putchar

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,13 +2,15 @@
 /**
  * main - counting
  * Return: success 0
- */ 
+ */
+
 	int main(void)
 {
 	int i;
 	i = 0;
-	while(i < 10) {
-	  printf("%d",i);
+	while
+	  (i < 10) {
+	  printf("%d", i);
 		i = i + 1;
 	}
 	printf("\n");

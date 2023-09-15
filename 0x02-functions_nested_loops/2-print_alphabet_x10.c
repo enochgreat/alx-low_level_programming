6d216089f8e4e1ff78dd85b4f23d6b194cce3129
enochgreat <enogreat18@gmail.com>
2023-09-14 17:27:24 -0700

    print x10 alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - Print alphabet
+ *
+ *Description: repeat ten times
+ *
+ *Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char i, j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+			_putchar(j);
+	}
+	_putchar('\n');
+	return (0);
+}

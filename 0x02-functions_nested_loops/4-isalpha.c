2422823647afd6770917db8dde40b1c18823d0e1
enochgreat <enogreat18@gmail.com>
2023-09-16 16:20:14 -0700

    both alpha & lower

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - print lower and upper case
+ *@c: return 1 or 0
+ *Return: 1 or 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a') && (c <= 'z'))
+		return (1);
+	else if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+
+}

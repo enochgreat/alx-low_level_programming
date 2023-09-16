87e6c16352fe8cc30c479cc94ed0cc4ba80431e1
enochgreat <enogreat18@gmail.com>
2023-09-16 15:39:15 -0700

    alphabet checker

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - print lower case
+ * @c: parameter returns 1 or 0
+ * Return: Always 0
+ *
+ */
+
+int _islower(int c)
+{
+	while
+		(c <= 'a' && c >= 'z')
+			return (1);
+	else
+		return (0);
+}

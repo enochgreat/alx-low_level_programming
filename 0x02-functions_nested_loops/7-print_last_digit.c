473072abf423a23eb4be2a87ab559749903a7087
enochgreat <enogreat18@gmail.com>
2023-09-16 18:50:30 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_last_digit - function to print last digit
+ * @i: takes in number
+ * Return: last digit
+ */
+
+int print_last_digit(int i)
+{
+	int j;
+
+	j = i % 10;
+	if (i < 0)
+	{
+		j = -j;
+		_putchar(j + '0');
+		return (j);
+	}
+
+	return (0);
+}

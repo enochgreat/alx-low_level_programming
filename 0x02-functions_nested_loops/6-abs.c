01aec11f8773d805d3f64a18b1100ff25b2cf9d0
enochgreat <enogreat18@gmail.com>
2023-09-16 17:36:49 -0700

    positve or negative

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_abs - print positive or negative numbers
+ *@c: a or b
+ *Return: 0 or 1
+ */
+
+int _abs(int b)
+{
+	if (b < 0)
+		return (-b);
+	else (b >= 0)
+		return (b);
+}

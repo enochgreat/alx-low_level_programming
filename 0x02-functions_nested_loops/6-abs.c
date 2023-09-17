5262c5c8f33384132284ae18720e7dfac90d5e3b
enochgreat <enogreat18@gmail.com>
2023-09-16 18:24:32 -0700

    absolute

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,17 @@
 #include "main.h"
 /**
- *_abs - print positive or negative numbers
- *@c: a or b
- *Return: 0 or 1
+ * _abs - print positive or negative numbers
+ * @b: a or b
+ * Return: 0 or 1
  */
 
 int _abs(int b)
 {
 	if (b < 0)
 		return (-b);
-	else (b >= 0)
+
+	else if (b >= 0)
+	{
 		return (b);
+	}
 }

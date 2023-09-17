af176313c47d98e85f652f05d5a37c8f22960e5a
enochgreat <enogreat18@gmail.com>
2023-09-16 18:04:39 -0700

    positive/negative

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,23 +1,23 @@
 #include "main.h"
 /**
- * print_sign - print +ve or -ve
- *@c: return answer
- *Return: 0 or 1
+ * print_sign - return both +ve or -ve
+ * @n: return parameter
+ * Return: 0 or 1
  */
 
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+')
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar(n + '0');
+		_putchar('0');
 		return (0);
 	}
-	else(n < 0)
+	else
 	{
 		_putchar('-');
 		return (-1);
